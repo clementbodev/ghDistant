f671ee152fa9b5d9b646fe499d5044a21afe8569
Clémentbo <80004461+clementbo50@users.noreply.github.com>
2025-05-01 15:19:40 +0200

    Close #4 changer la taille de la fenêtre d’origine et ajouter un label qui écrit la date du jour à coté du label helloworld (#5)
    * Close #1 Modification du fond en cyan
    
    * Close #4 changer la taille de la fenêtre d’origine et ajouter un label qui écrit la date du jour à coté du label helloworld
    
    * Close #3  changer la taille de la fenêtre d’origine
    
    * Close #4 changer la taille de la fenêtre d’origine et ajouter un label qui écrit la date du jour à coté du label helloworld
    
    ---------
    
    Co-authored-by: cbollengier <clementbodev@gmail.com>

--- public/js/HelloWorld.js
+++ public/js/HelloWorld.js
@@ -1,5 +1,8 @@
-const HELLO_WORLD_TITLE =  document.querySelector('h1');
+const HELLO_WORLD_TITLE = document.querySelector('h1');
 const HELLO_WORLD_TEXT = document.querySelector('p');
+const HELLO_WORLD_LABEL = document.querySelector('label');
+const date = new Date().toLocaleDateString('fr-FR');
 
 HELLO_WORLD_TITLE.innerText = 'Hello World';
 HELLO_WORLD_TEXT.innerText = 'This is a simple Hello World example using JavaScript.';
+HELLO_WORLD_LABEL.innerText = `Voici la date d'aujourd'hui : ${date}`;
